cb9a2bd1d97bf1d0fb1786ea26355a9034c27ff7
Stralu08 <Lukas.Straessler@gmx.at>
2016-04-15 12:03:41 +0200

    Code von Client und Server hinzugefuegt

---
+++ client/build/generated/helloworld/Echo.java
@@ -0,0 +1,13 @@
+package helloworld;
+
+/**
+ * Generated from IDL interface "Echo".
+ *
+ * @author JacORB IDL compiler V 3.7
+ * @version generated at Apr 15, 2016 10:31:08 AM
+ */
+
+public interface Echo
+	extends EchoOperations, org.omg.CORBA.Object, org.omg.CORBA.portable.IDLEntity
+{
+}
---
+++ client/build/generated/helloworld/EchoOperations.java
@@ -0,0 +1,16 @@
+package helloworld;
+
+
+/**
+ * Generated from IDL interface "Echo".
+ *
+ * @author JacORB IDL compiler V 3.7
+ * @version generated at Apr 15, 2016 10:31:08 AM
+ */
+
+public interface EchoOperations
+{
+	/* constants */
+	/* operations  */
+	java.lang.String echoString(java.lang.String mesg);
+}
